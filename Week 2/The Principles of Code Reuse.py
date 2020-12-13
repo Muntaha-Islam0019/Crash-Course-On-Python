0e3c6d0cb60361444a552037f81495c7f770eae7
Muntaha Islam <muntaha.islam0019@gmail.com>
2020-12-13 17:32:19 +0600

    Initial Commit

---
+++ Week 2/The Principles of Code Reuse.py
@@ -0,0 +1,13 @@
+# REPLACE THIS STARTER CODE WITH YOUR FUNCTION
+# june_days = 30
+# print("June has " + str(june_days) + " days.")
+# july_days = 31
+# print("July has " + str(july_days) + " days.")
+
+
+def month_days(month, days):
+    print(f"{month} has {days} days.")
+
+
+month_days("July", 31)
+month_days("June", 30)
